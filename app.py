a24e3ffcfc33385f38c1f387d8c5e7bffbc27642
seprimo22-bit <seprimo22@gmail.com>
2026-02-13 22:48:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,6 @@ def health():
 # Run locally
 # ---------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
