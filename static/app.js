1b7168fe202ffee7b85968661dc2ecef171f3244
seprimo22-bit <seprimo22@gmail.com>
2026-02-22 15:33:10 -0500

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,1 +1,14 @@
+function loadSpecials(day) {
+    fetch(`/get_specials/${day}`)
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById("results");
+            list.innerHTML = "";
 
+            data.forEach(item => {
+                const li = document.createElement("li");
+                li.innerText = `${item.bar_name} - ${item.deal}`;
+                list.appendChild(li);
+            });
+        });
+}
