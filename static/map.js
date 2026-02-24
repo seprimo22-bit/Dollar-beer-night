600f601337742f469abad2c00502bb4f8ff25fe5
seprimo22-bit <seprimo22@gmail.com>
2026-02-24 06:38:21 -0500

    Update map.js

--- static/map.js
+++ static/map.js
@@ -6,19 +6,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
 }).addTo(map);
 
 function clearMarkers(){
-    markers.forEach(m=>map.removeLayer(m));
-    markers=[];
+    markers.forEach(m => map.removeLayer(m));
+    markers = [];
 }
 
 function loadDay(day){
     fetch(`/get_specials/${day}`)
-    .then(r=>r.json())
-    .then(data=>{
+    .then(r => r.json())
+    .then(data => {
         clearMarkers();
-        const results=document.getElementById("results");
-        results.innerHTML="";
+        const results = document.getElementById("results");
+        results.innerHTML = "";
 
-        data.forEach(s=>{
+        data.forEach(s => {
+
+            // Deal card list
             results.innerHTML += `
                 <div class="deal-card">
                     <b>${s.bar_name}</b><br>
@@ -26,17 +28,27 @@ function loadDay(day){
                 </div>
             `;
 
+            // Map markers with navigation link
             if(s.latitude && s.longitude){
-                const m=L.marker([s.latitude,s.longitude])
+                const m = L.marker([s.latitude, s.longitude])
                     .addTo(map)
-                    .bindPopup(`<b>${s.bar_name}</b><br>${s.deal}`);
+                    .bindPopup(`
+                        <b>${s.bar_name}</b><br>
+                        ${s.deal}<br><br>
+                        <a href="https://www.google.com/maps/dir/?api=1&destination=${s.latitude},${s.longitude}"
+                           target="_blank">
+                           🧭 Navigate Here
+                        </a>
+                    `);
+
                 markers.push(m);
             }
         });
 
+        // Zoom map to markers
         if(markers.length){
-            const group=new L.featureGroup(markers);
-            map.fitBounds(group.getBounds(),{padding:[40,40]});
+            const group = new L.featureGroup(markers);
+            map.fitBounds(group.getBounds(), { padding:[40,40] });
         }
     });
 }
