73f27971fa2eee2c64807e49887a46aab8b37b05
seprimo22-bit <seprimo22@gmail.com>
2026-03-19 16:09:37 -0400

    Update map.js

--- static/map.js
+++ static/map.js
@@ -1,18 +1,18 @@
 let map, vectorSource, vectorLayer, popupOverlay;
 
+// Initialize the map
 function initMap() {
-    // Create a vector source and layer for bar markers
     vectorSource = new ol.source.Vector({});
     vectorLayer = new ol.layer.Vector({ source: vectorSource });
 
-    // Create popup overlay
+    // Create popup container
     const container = document.createElement("div");
     container.id = "popup";
     container.style.backgroundColor = "white";
-    container.style.padding = "8px";
+    container.style.padding = "6px";
     container.style.border = "1px solid black";
-    container.style.borderRadius = "6px";
-    container.style.minWidth = "150px";
+    container.style.borderRadius = "4px";
+    container.style.minWidth = "140px";
     container.style.position = "absolute";
     container.style.display = "none";
     document.body.appendChild(container);
@@ -24,7 +24,6 @@ function initMap() {
         offset: [0, -12]
     });
 
-    // Initialize the map
     map = new ol.Map({
         target: "map",
         layers: [
@@ -32,27 +31,25 @@ function initMap() {
             vectorLayer
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat([-84.5555, 41.0379]), // Default center
+            center: ol.proj.fromLonLat([-84.5555, 41.0379]), // default center
             zoom: 10
         }),
         overlays: [popupOverlay]
     });
 
-    // Click on marker to show popup with Navigate link
+    // Click on marker to show popup
     map.on("singleclick", function(evt) {
         const feature = map.forEachFeatureAtPixel(evt.pixel, f => f);
         if (feature) {
             const coords = feature.getGeometry().getCoordinates();
-            popupOverlay.setPosition(coords);
-
             const name = feature.get("name");
             const deal = feature.get("deal");
-            const lat = feature.get("lat");
-            const lng = feature.get("lng");
+            const address = feature.get("address");
 
-            const navUrl = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
-
-            container.innerHTML = `<b>${name}</b><br>${deal}<br><a href="${navUrl}" target="_blank">Navigate</a>`;
+            popupOverlay.setPosition(coords);
+            container.innerHTML = `<b>${name}</b><br>${deal}<br>
+                <a href="https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(address)}" target="_blank">
+                Navigate</a>`;
             container.style.display = "block";
         } else {
             container.style.display = "none";
@@ -60,7 +57,7 @@ function initMap() {
     });
 }
 
-// Load bars for a given day and add markers
+// Load bars for the current day
 function loadBars(bars) {
     vectorSource.clear();
 
@@ -70,51 +67,33 @@ function loadBars(bars) {
                 geometry: new ol.geom.Point(ol.proj.fromLonLat([bar.lng, bar.lat])),
                 name: bar.bar_name,
                 deal: bar.deal,
-                lat: bar.lat,
-                lng: bar.lng
+                address: bar.address || bar.bar_name
             });
 
-            // Use a standard pin icon instead of a circle
-            const iconStyle = new ol.style.Style({
-                image: new ol.style.Icon({
-                    anchor: [0.5, 1],
-                    src: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
-                    scale: 0.05
+            const style = new ol.style.Style({
+                image: new ol.style.Circle({
+                    radius: 8,
+                    fill: new ol.style.Fill({ color: '#1976d2' }),
+                    stroke: new ol.style.Stroke({ color: '#fff', width: 2 })
                 })
             });
 
-            feature.setStyle(iconStyle);
+            feature.setStyle(style);
             vectorSource.addFeature(feature);
         }
     });
-
-    // Optionally auto-zoom to fit all markers
-    if (vectorSource.getFeatures().length > 0) {
-        const extent = vectorSource.getExtent();
-        map.getView().fit(extent, { padding: [50, 50, 50, 50], maxZoom: 14 });
-    }
 }
 
-// Focus map on a specific bar when clicking the list
+// Focus map on a bar from the list
 function focusBar(bar) {
     if (bar.lat && bar.lng) {
-        const coords = ol.proj.fromLonLat([bar.lng, bar.lat]);
-        map.getView().animate({ center: coords, zoom: 14, duration: 500 });
-
-        // Trigger popup for that bar
-        const feature = vectorSource.getFeatures().find(f => f.get("name") === bar.bar_name && f.get("deal") === bar.deal);
-        if (feature) {
-            popupOverlay.setPosition(feature.getGeometry().getCoordinates());
-            const navUrl = `https://www.google.com/maps/dir/?api=1&destination=${bar.lat},${bar.lng}`;
-            document.getElementById("popup").innerHTML = `<b>${bar.bar_name}</b><br>${bar.deal}<br><a href="${navUrl}" target="_blank">Navigate</a>`;
-            document.getElementById("popup").style.display = "block";
-        }
+        const view = map.getView();
+        view.animate({ center: ol.proj.fromLonLat([bar.lng, bar.lat]), zoom: 14, duration: 500 });
     }
 }
 
-// Expose functions globally for index.html
+// Bind functions globally so index.html can use them
 window.loadBars = loadBars;
 window.focusBar = focusBar;
 
-// Initialize map
 initMap();
