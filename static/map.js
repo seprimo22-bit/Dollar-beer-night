1003e97f3839498e9a61efc5919a61c5ce968163
seprimo22-bit <seprimo22@gmail.com>
2026-03-19 15:30:12 -0400

    Update map.js

--- static/map.js
+++ static/map.js
@@ -4,7 +4,6 @@ function initMap() {
     vectorSource = new ol.source.Vector({});
     vectorLayer = new ol.layer.Vector({ source: vectorSource });
 
-    // Popup div
     const container = document.createElement("div");
     container.id = "popup";
     container.style.backgroundColor = "white";
@@ -13,7 +12,7 @@ function initMap() {
     container.style.borderRadius = "4px";
     container.style.position = "absolute";
     container.style.display = "none";
-    container.style.minWidth = "120px";
+    container.style.minWidth = "140px";
     document.body.appendChild(container);
 
     popupOverlay = new ol.Overlay({
@@ -30,21 +29,31 @@ function initMap() {
             vectorLayer
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat([-84.5555, 41.0379]), // default center
+            center: ol.proj.fromLonLat([-84.5555, 41.0379]),
             zoom: 10
         }),
         overlays: [popupOverlay]
     });
 
-    // Click on marker to show popup
+    // Click map feature to show popup
     map.on("singleclick", function(evt) {
         const feature = map.forEachFeatureAtPixel(evt.pixel, f => f);
         if (feature) {
             const coords = feature.getGeometry().getCoordinates();
             popupOverlay.setPosition(coords);
+
             const name = feature.get("name");
             const deal = feature.get("deal");
-            container.innerHTML = `<b>${name}</b><br>${deal}`;
+            const lat = feature.get("lat");
+            const lng = feature.get("lng");
+
+            // Detect OS for map link
+            const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+            const navUrl = isIOS
+                ? `https://maps.apple.com/?daddr=${lat},${lng}&dirflg=d`
+                : `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+
+            container.innerHTML = `<b>${name}</b><br>${deal}<br><a href="${navUrl}" target="_blank">Navigate</a>`;
             container.style.display = "block";
         } else {
             container.style.display = "none";
@@ -59,13 +68,15 @@ function loadBars(bars) {
             const feature = new ol.Feature({
                 geometry: new ol.geom.Point(ol.proj.fromLonLat([bar.lng, bar.lat])),
                 name: bar.bar_name,
-                deal: bar.deal
+                deal: bar.deal,
+                lat: bar.lat,
+                lng: bar.lng
             });
 
             const style = new ol.style.Style({
                 image: new ol.style.Circle({
                     radius: 8,
-                    fill: new ol.style.Fill({ color: '#ff0000' }),
+                    fill: new ol.style.Fill({ color: '#ff5722' }),
                     stroke: new ol.style.Stroke({ color: '#fff', width: 2 })
                 })
             });
