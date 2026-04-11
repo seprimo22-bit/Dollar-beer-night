c39851c03c5e0a035278b680b1aaf4f53bc671d7
seprimo22-bit <seprimo22@gmail.com>
2026-04-10 20:53:50 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,37 +1,39 @@
-let map, autocomplete, selectedPlace;
+let map;
+let markers = [];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 13, center: { lat: 41.099, lng: -80.649 }
+        zoom: 12,
+        center: { lat: 41.0998, lng: -80.6495 }, // Youngstown
+        disableDefaultUI: true
     });
-
-    // Auto-search for adding bars
-    const input = document.getElementById("bar-search");
-    if (input) {
-        autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.addListener("place_changed", () => {
-            selectedPlace = autocomplete.getPlace();
-        });
-    }
     loadBars();
 }
 
 async function loadBars(day = "") {
     const res = await fetch(`/api/specials?day=${day}`);
-    const bars = await res.json();
+    const data = await res.json();
     const list = document.getElementById("specialsList");
     list.innerHTML = "";
 
-    bars.forEach(bar => {
-        const div = document.createElement("div");
-        div.className = "card";
-        div.innerHTML = `<strong>${bar.name}</strong><br><span style="color:green;">${bar.special}</span><br><small>${bar.address}</small>`;
-        list.appendChild(div);
-        
-        new google.maps.Marker({
+    // Clear old markers
+    markers.forEach(m => m.setMap(null));
+    markers = [];
+
+    data.forEach(bar => {
+        // Add to List
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `<strong>${bar.name}</strong><br><span style="color:green;">${bar.special}</span><br><small>${bar.address}</small>`;
+        list.appendChild(card);
+
+        // Add to Map
+        const marker = new google.maps.Marker({
             position: { lat: bar.lat, lng: bar.lng },
             map: map,
             title: bar.name
         });
+        markers.push(marker);
     });
 }
+
